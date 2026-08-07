caf6d84cfcd0809ea79109fa718cd926f95e3ef5
VTU29103 <hasini.kw07@gmail.com>
2026-08-07 09:57:56 +0530

    Add files via upload

--- week 2/session61.java
+++ week 2/session61.java
@@ -1,1 +1,11 @@
-jhkjhuhg
+class Solution {
+    public int[] buildArray(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            ans[i] = nums[nums[i]];
+        }
+
+        return ans;
+    }
